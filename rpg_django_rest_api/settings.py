0afbdce2ab5905ef3b915dd3ca6f8a766cefac15
Gabriel Esteves <38438278+ichbingabe@users.noreply.github.com>
2022-08-27 20:00:27 -0300

    Update settings.py

--- rpg_django_rest_api/settings.py
+++ rpg_django_rest_api/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 import environ
 
+env = environ.Env()
+environ.Env.read_env(f"{BASE_DIR}\.env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '' #put your secret key here
+SECRET_KEY = env('SECRET_KEY') #put your secret key here
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
