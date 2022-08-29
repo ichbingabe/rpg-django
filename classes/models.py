059c8072423c956fc4ff192c0fd0cb04e2b3a473
Gabriel Esteves <38438278+ichbingabe@users.noreply.github.com>
2022-08-29 19:06:09 -0300

    Add models

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class playerModel(models.Model):
+    player_class = models.CharField(max_length=100)
+    player_subclass = models.CharField(max_length=100)
+    abreviation = models.CharField(max_length=100)
+    equipments = models.CharField(max_length=100)
+    strength = models.CharField(max_length=100)
+    constituition = models.CharField(max_length=100)
+    dexterity = models.CharField(max_length=100)
+    intelligence = models.CharField(max_length=100)
+    wisdom = models.CharField(max_length=100)
+    charisma = models.CharField(max_length=100)
